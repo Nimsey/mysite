7518ecbd0110ff1e0e88a1c2022f8ff8fad88789
susan stephens <crazeygrafix@gmail.com>
2024-02-09 12:48:14 -0700

    fix: got the views to show and update

--- polls/views.py
+++ polls/views.py
@@ -20,32 +20,32 @@ client = MongoClient(os.getenv('MONGO_URI'))
 db = client.polls
 collection = db.polls_question
 
-print('----Availiable Collections----')
-for collection in db.list_collection_names():
-    print('---> '+collection)
-print('')
-print('----Availiable Questions----')
-all_questions = db.polls_question.find()
-q_num = 1
-for q in all_questions:
-    print('Question number '+str(q_num)+': '+q['question_text']+' (Published on: '+str(q['pub_date'])+')')
-    q_num += 1
-print('')
+# print('----Availiable Collections----')
+# for collection in db.list_collection_names():
+#     print('---> '+collection)
+# print('')
+# print('----Availiable Questions----')
+# all_questions = db.polls_question.find()
+# q_num = 1
+# for q in all_questions:
+#     print('Question number '+str(q_num)+': '+q['question_text']+' (Published on: '+str(q['pub_date'])+')')
+#     q_num += 1
+# print('')
 
-new_q = {"question_text": "What's new?", "pub_date": datetime.now()}
+# new_q = {"question_text": "What's new?", "pub_date": datetime.now()}
 
-does_new_q_exist_in_db = False
+# does_new_q_exist_in_db = False
 
-for q in db.polls_question.find():
-    if q['question_text'] == new_q['question_text']:
-        does_new_q_exist_in_db = True
-        break
-if not does_new_q_exist_in_db:
-    db.polls_question.insert_one(new_q)
-    print('New question added to the database!')
-else:
-    print('"'+new_q["question_text"]+'" already exists in the database!')
-print('')
+# for q in db.polls_question.find():
+#     if q['question_text'] == new_q['question_text']:
+#         does_new_q_exist_in_db = True
+#         break
+# if not does_new_q_exist_in_db:
+#     db.polls_question.insert_one(new_q)
+#     print('New question added to the database!')
+# else:
+#     print('"'+new_q["question_text"]+'" already exists in the database!')
+# print('')
 
 def update_question(question_id, new_question_text):
     db.polls_question.update_one({"_id": ObjectId(question_id)}, {"$set": {"question_text": new_question_text}})
