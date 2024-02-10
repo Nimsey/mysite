c34e20a5a273e9a719cde69c2b7904b70be0a035
susan stephens <crazeygrafix@gmail.com>
2024-02-10 16:23:57 -0700

    feat: completed unit 6 and 7

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,5 @@
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -7,6 +7,11 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def __str__(self):
         return self.question_text
@@ -19,3 +24,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
